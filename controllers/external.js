6d334356c850612efe75e6c2c3ad13a0be261310
Trey <treyw1850@gmail.com>
2018-08-31 23:35:13 -0400

    new canvas

--- controllers/external.js
+++ controllers/external.js
@@ -1,21 +1,11 @@
-const imageToSlices = require('image-to-slices');
-const canvas = require('canvas')
 const shuffle = require('shuffle-array');
 
 module.exports = {
 
-    // READ all accounts from the database
+    // slice the image
     sliceImage: (req, res) => {
         console.log(req.body.img)
-        var lineXArray = [100, 200];
-        var lineYArray = [100, 200];
-        var source = req.body.img; 
-
-        imageToSlices(source, lineXArray, lineYArray, {
-            saveToDir: req.body.img
-        }, function () {
-            console.log('the source image has been sliced into 9 sections!');
-        });
+        
     }
 
 }
--- views/assets/js/api.js
+++ views/assets/js/api.js
@@ -1,24 +1,27 @@
-sliceItUp = () => {
-    const postPic = document.getElementById('Puzzle_img-js')
-    doTheFetch(postPic)   
-    console.log(postPic)
-} 
+// sliceItUp = () => {
+//     const postPic = document.getElementById('Puzzle_img-js')
+//     doTheFetch(postPic)   
+//     console.log(postPic)
+// } 
 
-doTheFetch = (arg) => {
-    fetch('http://localhost:4040/api/split', {
-        method: 'post',
-        body: JSON.stringify({
-            img: arg.src
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-      })
-      .then((res) => {
-        return res.json();
-      })
-      .catch(function (error) {  
-        console.log('Request failure: ', error);  
-      });
-    console.log(arg)
-}
+// doTheFetch = (arg) => {
+//     fetch('http://localhost:4040/api/split', {
+//         method: 'post',
+//         body: JSON.stringify({
+//             img: arg.src
+//         }),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//       })
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .catch(function (error) {  
+//         console.log('Request failure: ', error);  
+//       });
+//     console.log(arg)
+// }
+
+const canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d')
